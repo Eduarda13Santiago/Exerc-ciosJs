f2f4e52157ea695ccc5b1dc5abdb2a5bae7783d5
EDUARDA VICTORIA SANTIAGO CELESTINO <43620385823@info.local>
2024-05-09 10:51:03 -0300

    feat:exercicio 3

--- ex2.js
+++ ex2.js
@@ -5,29 +5,29 @@
 
 //solicita ao usuário um número de 1 a 7 representando um dia da semana
 
-let diadasemana  = parseInt(prompt("Digite um número de 1 a 7 para representar um dia da semana :"));
+//let diadasemana  = parseInt(prompt("Digite um número de 1 a 7 para representar um dia da semana :"));
 
-if(diadasemana == 1 ){
-    console.log("Segunda-feira")
-}
-if(diadasemana == 2 ){
-    console.log("Terça-feira")
-}
-if(diadasemana == 3 ){
-    console.log("Quarta-feira")
-}
-if(diadasemana == 4 ){
-    console.log("Quinta-feira")
-}
-if(diadasemana == 5 ){
-    console.log("Sexta-feira")
-}
-if(diadasemana == 6 ){
-    console.log("Sabádo")
-}
-if(diadasemana == 7 ){
-    console.log("Domingo")
-}
+// if(diadasemana == 1 ){
+//     console.log("Segunda-feira")
+// }
+// if(diadasemana == 2 ){
+//     console.log("Terça-feira")
+// }
+// if(diadasemana == 3 ){
+//     console.log("Quarta-feira")
+// }
+// if(diadasemana == 4 ){
+//     console.log("Quinta-feira")
+// }
+// if(diadasemana == 5 ){
+//     console.log("Sexta-feira")
+// }
+// if(diadasemana == 6 ){
+//     console.log("Sabádo")
+// }
+// if(diadasemana == 7 ){
+//     console.log("Domingo")
+// }
 
 
 
---
+++ ex3.js
@@ -0,0 +1,7 @@
+//Exercício 3: Ímpar ou par
+//Faça um programa que receba um número do usuário e informe se este número é par ou
+//ímpar.
+
+
+let number = parseInt(prompt("Coloque um número inteiro e informaremos se este número é par ou ímpar: "))
+

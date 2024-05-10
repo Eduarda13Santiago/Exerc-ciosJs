fb094b4a6f382d2fc9e6d0b581dca77c4a9bfabf
EDUARDA VICTORIA SANTIAGO CELESTINO <43620385823@info.local>
2024-05-10 08:55:20 -0300

    feat:exercicio 3

--- ex3.js
+++ ex3.js
@@ -2,6 +2,12 @@
 //Faça um programa que receba um número do usuário e informe se este número é par ou
 //ímpar.
 
+ let number = prompt("Coloque um número inteiro e informaremos se este número é par ou ímpar: ")
 
-let number = parseInt(prompt("Coloque um número inteiro e informaremos se este número é par ou ímpar: "))
+if (number % 2 === 0) {
+    console.log("O número é par.");
+ } 
 
+ else {
+   console.log("O número é ímpar.");
+ }

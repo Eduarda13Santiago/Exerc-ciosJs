f953df6f72e924a01adb1d3e713541442bce015b
FIC <fic@info.local>
2024-05-13 15:19:17 -0300

    Ex 1,2,3,4,5,6,7

--- loop/ex6.js
+++ loop/ex6.js
@@ -1,12 +1,12 @@
 // Imprimir números pares de 0 a 20 utilizando o loop while
 
-let num = 0;
+// let num = 0;
 
-console.log("Números pares de 0 a 20: ");
+// console.log("Números pares de 0 a 20: ");
 
-while(num <= 20){
-    if(num % 2 == 0){
-        console.log(num)
-    }
-    num ++;
-}
+// while(num <= 20){
+//     if(num % 2 == 0){
+//         console.log(num)
+//     }
+//     num ++;
+// }

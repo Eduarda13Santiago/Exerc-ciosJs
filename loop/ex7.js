e801d967dd44133c2382b962c95606d8aca15fa5
FIC <fic@info.local>
2024-05-13 15:23:51 -0300

    Ex 1,2,3,4,5,6,7,8

--- loop/ex7.js
+++ loop/ex7.js
@@ -1,12 +1,12 @@
 // Calcular a soma dos números de 1 a 100 utilizando a estrutura
 // while.
 
-let num = 1;
-let soma = 0;
+// let num = 1;
+// let soma = 0;
 
-while(num <= 100){
-    soma += num;
-    num++;
-}
+// while(num <= 100){
+//     soma += num;
+//     num++;
+// }
 
-console.log("A soma dos números de 1 a 100 é: " + soma);
+// console.log("A soma dos números de 1 a 100 é: " + soma);

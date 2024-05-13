168d0aaeb1d4b2ee6c2987c5910aa12d1174ee11
FIC <fic@info.local>
2024-05-13 14:42:48 -0300

    Exercicio 2

---
+++ loop/ex2.js
@@ -0,0 +1,15 @@
+// 2. Imprimir numeros pares de 1 a 10.
+
+// console.log("--------------------------------")
+
+// for(let i = 2; i <= 10; i += 2){
+//     console.log(i);
+// }
+
+
+
+for (let num = 1; num <= 10; num++){
+    if(num % 2 == 0){
+        console.log(num);
+    }
+}
